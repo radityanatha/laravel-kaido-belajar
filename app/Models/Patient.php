8b7b44ff050e044e1939ac60ce86dcdacbbad5ef
radityanatha <radityanatha20@gmail.com>
2025-05-25 08:57:13 +0800

    Menambahkan protected fillable dan pengguna hasfactory pada models-patient dan migrations

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Patient extends Model
 {
     //
-
+    use HasFactory;
     // add fillable
-    protected $fillable = [];
+    protected $fillable = [
+        'nik',
+        'name',
+        'gender',
+        'date_of_birth',
+        'address',
+    ];
     // add guaded
     protected $guarded = ['id'];
     // add hidden
