50b1936e4bdd032ebf1fd7adcd288cbc18d4e482
radityanatha <radityanatha20@gmail.com>
2025-05-25 14:42:05 +0800

    Perbaikan Auth mendefenisikan tidak ganggu library global yang lain

--- app/Filament/Resources/PatientAppointmentResource.php
+++ app/Filament/Resources/PatientAppointmentResource.php
@@ -12,7 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use auth;
+use Illuminate\Support\Facades\Auth;
 
 class PatientAppointmentResource extends Resource
 {
